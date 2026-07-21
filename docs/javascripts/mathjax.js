c31d25eead6f43b9d07f0226fd26f056e435d168
Prajwal Bharadwaj <133301513+prajwal1798@users.noreply.github.com>
2026-07-21 18:35:06 +0100

    Harden MathJax rendering for instant navigation

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -12,5 +12,15 @@ window.MathJax = {
 };
 
 document$.subscribe(() => {
+  if (!window.MathJax || !MathJax.typesetPromise) {
+    return;
+  }
+
+  if (MathJax.startup && MathJax.startup.output) {
+    MathJax.startup.output.clearCache();
+  }
+
+  MathJax.typesetClear();
+  MathJax.texReset();
   MathJax.typesetPromise();
 });
